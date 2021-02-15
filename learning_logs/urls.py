c140d56ff024c91df8ce70575c94857306c3b92a
lect <litaniasr@gmail.com>
2021-02-16 00:24:52 +0300

    Added functionality for adding new topics and new entries for each topic.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,5 +12,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page with all the topics created by user.
     path('topics/', views.topics, name='topics'),
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    # Page with all the entries for certain topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page with a form for creating new topic.
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page with a form for creating new entry for some topic.
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -20,3 +21,30 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Defines new topic."""
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Defines new entry for a certain topic."""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    context = {'form': form, 'topic': topic}
+    return render(request, 'learning_logs/new_entry.html', context)
