4f87211b24857e66ad4161df3cc842a1018877e6
lect <litaniasr@gmail.com>
2021-02-16 00:54:41 +0300

    Added functionality for editing entries for each topic.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -17,5 +17,6 @@ urlpatterns = [
     # Page with a form for creating new topic.
     path('new_topic/', views.new_topic, name='new_topic'),
     # Page with a form for creating new entry for some topic.
-    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry')
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
