a01d945f13c50c3e680deb4090337a35c584db48
lect <litaniasr@gmail.com>
2021-02-15 17:08:10 +0300

    Added url pattern, view and template for home page of learning_logs app

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+"""
+Sets URL patterns for learning_logs app.
+"""
+
+from django.urls import path
+from . import views
+
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Home page.
+    path('', views.index, name='index'),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """Home page of learning_logs app."""
+    return render(request, 'learning_logs/index.html')
