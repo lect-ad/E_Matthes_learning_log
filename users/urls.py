fbf9a8c3d7139c832be4fb6c4f3661b90c41cfde
lect <litaniasr@gmail.com>
2021-02-16 03:58:57 +0300

    Added users app. Added registration functionality.

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,13 @@ Sets URL patterns for users app.
 
 from django.urls import path, include
 
+from . import views
+
 
 app_name = 'users'
 urlpatterns = [
-    # Django default log-in page.
+    # Django default authentication pages.
     path('', include('django.contrib.auth.urls')),
+    # Registration page.
+    path('register/', views.register, name='register')
 ]
